fa13c82552f8ffa12f8c0f3ac7976fd93d241857
Marijn van der Zee <marijn@serraict.com>
2012-12-07 12:38:29 +0100

    Add plain test, which passes without any modifications to production code
    The _same_ test fails in my environment, so:
    
     * The issue was resolved in the current master branch, or
     * (more likely) there is something special about my situation
    
    Will investigate further:
    
     * are my event routers special? IDispatchCommits?
     * do I hook into the persistence engine without realizing it?

---
+++ src/EventStore.NUnitTest_issue_143/EventsShouldNotBePersistedBetweenTestRuns_Issue143.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EventStore.NUnitTest_issue_143
+{
+    [TestFixture]
+    public class EventsShouldNotBePersistedBetweenTestRuns_Issue143 : FullContextTestBase
+    {
+        Guid _streamId = new Guid("CC0FB6B5-9400-4070-B613-D90339025142");
+
+        [Test]
+        public void One_AddOneStreamAndMakeSureThereIsExactlyOneInStoreWhenDone()
+        {
+            using (var stream = Store.OpenStream(_streamId, 0, int.MaxValue))
+            {
+                Assert.AreEqual(0, stream.CommittedEvents.Count);
+
+                var @event = new SomeDomainEvent {Value = "One"};
+
+                stream.Add(new EventMessage {Body = @event});
+                stream.CommitChanges(Guid.NewGuid());
+
+                Assert.AreEqual(1, stream.CommittedEvents.Count);
+            }
+        }
+
+        [Test]
+        public void Two_AgainAddOneStreamAndMakeSureThereIsExactlyOneInStoreWhenDone()
+        {
+            using (var stream = Store.OpenStream(_streamId, 0, int.MaxValue))
+            {
+                Assert.AreEqual(0, stream.CommittedEvents.Count);
+                var @event = new SomeDomainEvent { Value = "One" };
+
+                stream.Add(new EventMessage { Body = @event });
+                stream.CommitChanges(Guid.NewGuid());
+                Assert.AreEqual(1, stream.CommittedEvents.Count);
+            }
+
+        }
+    }
+
+    public class FullContextTestBase
+    {
+        protected IStoreEvents Store;
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            Store = GetInMemoryEventStore();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //_store.Advanced.Purge();
+            Store.Dispose();
+        }
+
+        private IStoreEvents GetInMemoryEventStore()
+        {
+            return Wireup.Init()
+                         .LogToOutputWindow()
+                         .UsingInMemoryPersistence()
+                         .UsingSynchronousDispatchScheduler()
+                         .Build();
+        }
+    }
+
+    public class SomeDomainEvent
+    {
+        public string Value { get; set; }
+    }
+}
